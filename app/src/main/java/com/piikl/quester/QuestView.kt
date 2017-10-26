46ac021392ab0b948ce0270e3e71eeca219985f5
Caleb Hiebert <calebkhiebert@gmail.com>
2017-10-25 21:14:10 -0500

    create quest view activity

---
+++ app/src/main/java/com/piikl/quester/QuestView.kt
@@ -0,0 +1,12 @@
+package com.piikl.quester
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QuestView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quest_view)
+    }
+}
